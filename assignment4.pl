a6aa983329fc08892f2a380d68678b465a930901
Chesypofs <whodgson@ualberta.ca>
2017-04-02 11:03:34 -0600

    Fixed disarm edgecase and comment updates

--- assignment4.pl
+++ assignment4.pl
@@ -50,15 +50,20 @@ fourSquares(N, [S1, S2, S3, S4]) :-
  less than or equal to the next months dismantlements.
  
  Examples:
+	Adivisions: 1,1
+	Bdivisions: 1,1
+	Solution: false
+	
 	Adivisions: 1,3,3,4,6,10,12
 	Bdivisions: 3,4,7,9,16
 	Solution: [[[1,3],[4]],[[3,4],[7]],[[12],[3,9]],[[6,10],[16]]]
 	
-	Adivisions:
-	Bdivisions:
-	Solution:
+	Adivisions: 1,2,3,3,8,5,5
+	Bdivisions: 3,6,4,4,10
+	Solution: [[[1, 2], [3]], [[3, 3], [6]], [[8], [4, 4]], [[5, 5], [10]]]
 --------------------------------------------------------- */
 
+disarm([],[],[]).
 disarm(Adivisions, Bdivisions, Solution) :-
 	length(Adivisions, An),
 	length(Bdivisions, Bn),
